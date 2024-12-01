44a1bc63b51ff84755a082d0a2c68f8714a63c7e
Basel Hussein <cv98@windowslive.com>
2024-12-01 07:25:03 +0000

    created urls links

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(requests):
+    return HttpResponse('<h2>Home</h2>')
+
+def posts(requests):
+    return HttpResponse('<h2>Posts</h2>')
+
+def post(requests):
+    return HttpResponse('<h2>Post title</h2>')
+
+def profile(requests):
+    return HttpResponse('<h2>User profile</h2>')
